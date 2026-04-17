5f83725ed92b3f0c48db3c6ee51e2fecb18cf26a
nitflame <kartiknitinsetamraju@gmail.com>
2026-04-17 12:11:59 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/AI-network-System/', // <--- ADD THIS LINE HERE
   plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
